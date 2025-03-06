885ddc4014be5123768f2cb5f7e8f74e503770e4
hatayoshiki <yoshikihata1@gmail.com>
2025-03-06 20:29:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,14 @@ origins = [
     "http://127.0.0.1:3000"
 ]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # ✅ ルーターの登録
 router = APIRouter()
 app.include_router(products.router)
